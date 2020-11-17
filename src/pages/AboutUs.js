e5853970c9b93238d2f509611406d0fc607cb982
eita-tachino <34756983+eita-tachino@users.noreply.github.com>
2020-11-17 13:24:08 +0900

    4. FaqSection

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,12 +2,14 @@ import React from 'react'
 // Page Components
 import AboutSection from '../components/AboutSection'
 import ServisesSection from '../components/ServicesSection'
+import FaqSection from '../components/FaqSection'
 
 const AboutUs = () => {
     return(
     <>
         <AboutSection/>
         <ServisesSection/>
+        <FaqSection/>
     </>
     )
 }
