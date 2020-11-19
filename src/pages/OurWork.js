7ad6736f60a475a22e86c15ea3d3348a81926bd6
eita-tachino <34756983+eita-tachino@users.noreply.github.com>
2020-11-19 10:13:32 +0900

    13. Our Work Animation Section

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -7,15 +7,25 @@ import leaves from '../img/emb-02.jpg'
 import mesh from '../img/emb-Net.jpg'
 // Animations
 import {motion} from 'framer-motion'
-import {pageAnimation} from '../animation'
+import {sliderContainer,slider, pageAnimation, fade, photoAnim, lineAnim} from '../animation'
 
 const OurWork = () => {
     return (
         <Work exit="exit" variants={pageAnimation} initial="hidden" animate="show" style={{background: '#fff'}}>
+            <motion.div variants={sliderContainer}>
+                <Frame1 variants={slider}></Frame1>
+                <Frame2 variants={slider}></Frame2>
+                <Frame3 variants={slider}></Frame3>
+                <Frame4 variants={slider}></Frame4>
+            </motion.div>
             <Movie>
-                <h2>The Feather Works</h2>
-                <div className="line"></div>
-                <Link to="/work/the-feather"><img src={feather} alt="feather"/></Link>
+                <motion.h2  variants={fade}>The Feather Works</motion.h2>
+                <motion.div variants={lineAnim} className="line"></motion.div>
+                <Link to="/work/the-feather">
+                    <Hide>
+                        <motion.img variants={photoAnim} src={feather} alt="feather"/>
+                    </Hide>
+                </Link>
             </Movie>
             <Movie>
                 <h2>The Leaves Works</h2>
@@ -43,7 +53,7 @@ const Movie = styled.div`
     padding-bottom: 10rem;
     .line{
         height: 0.5rem;
-        background: #ccc;
+        background: #23d997;
         margin-bottom: 3rem;
     }
     img{
@@ -52,5 +62,26 @@ const Movie = styled.div`
         object-fit: cover;
     }
 `
-
+const Hide = styled.div`
+    overflow: hidden;
+`
+// Frame Animation
+const Frame1 =styled(motion.div)`
+    position: fixed;
+    left: 0;
+    top: 10%;
+    width: 100%;
+    height: 100vh;
+    background: #fffebf;
+    z-index: 2;
+`
+const Frame2 = styled(Frame1)`
+    background: #ff8efb;
+`
+const Frame3 = styled(Frame1)`
+    background: #8ed2ff;
+`
+const Frame4 = styled(Frame1)`
+    background: #8effa0;
+`
 export default OurWork
